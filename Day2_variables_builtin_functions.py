552f39b2ea63bc3148aedd21b7acc772d5808753
saadshr <saad20azmm@gmail.com>
2026-07-10 19:06:47 +0100

    Day2_variables_builtin_fucntions.py Exercice: level2 part1 sol

--- Day2_variables_builtin_functions.py
+++ Day2_variables_builtin_functions.py
@@ -39,3 +39,31 @@ first_name = 'Saad'
 print(first_name)               
 first_name_to_list = list(first_name)
 print(first_name_to_list)  
+#💻 Exercises - Day 2
+#Exercises: Level 1
+#1. Done 
+#2.Write a python comment saying 'Day 2: 30 Days of python programming'
+# Day 2: 30 Days of python programming
+#3.Declare a first name variable and assign a value to it
+first_name = 'Saad'
+#4.Declare a last name variable and assign a value to it
+last_name = 'Sahraoui'
+#5.Declare a full name variable and assign a value to it
+full_name = first_name + ' ' + last_name
+#6.Declare a country variable and assign a value to it
+country = 'Morocco'
+#7.Declare a city variable and assign a value to it
+city = 'Tangier'
+#8.Declare an age variable and assign a value to it
+age = 21
+#9.Declare a year variable and assign a value to it
+year = 2024
+#10.Declare a variable is_married and assign a value to it
+is_married = False 
+#11.Declare a variable is_true and assign a value to it
+is_true = True 
+#12.Declare a variable is_light_on and assign a value to it
+is_light_on = True
+#  13.Declare multiple variable on one line
+first_name, last_name, country, age, is_married = 'Saad', 'Sahraoui', 'Morocco', 21, False
+#Exercises: Level 2 
