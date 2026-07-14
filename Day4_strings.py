4070bdb4aad0aff9b853fc1e738bdd247f0e8859
saadshr <saad20azmm@gmail.com>
2026-07-14 02:04:36 +0100

    Day4_strings.py update ex1

--- Day4_strings.py
+++ Day4_strings.py
@@ -1,2 +1,9 @@
 #---------------Day4_strings.py----------------- 
-print("Strings Exercices in Python :")
+#----------------💻 Exercises - Day 4------------------------ 
+# 1 : Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+a = "Thirty"
+b = "Days"
+c = "Of"
+d = "Python"
+conca = a + b + c + d 
+print("Reultat de la concatenation : " , conca)
