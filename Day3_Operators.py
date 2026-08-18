e0e3569aa435a43ad086196b68af289b53a56d07
saadshr <saad20azmm@gmail.com>
2026-08-18 12:03:38 +0100

    Day3-Operators 1 to 7

--- Day3_Operators.py
+++ Day3_Operators.py
@@ -1,6 +1,35 @@
 #------------------💻 Exercises - Day 3--------------------
 #1 : Declare your age as integer variable
-print("SAAD")
+age = 21 
+print(age)
+print(type(age))
+#2 : declare your height as a float variable
+height = 1.70 
+print(height)
+print(type(height))
+#3 : declare a complex number variable
+complex_number = 3 + 4j 
+print(complex_number)
+print(type(complex_number))
+#4 : write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
+base = float(input("Enter base :"))
+height = float(input("Enter height : "))
+area = 0.5 * base * height 
+print("The area of the triangle is :", area)
+#5 : write a script that prompts the user to enter side a, side b, and side c of the triangle and calculate the perimeter of the triangle (perimeter = a + b + c)
+side_a = int(input("Enter side a :"))
+side_b = int(input("Enter side b :"))
+side_c = int(input("Enter side c :"))
+perimeter = side_a + side_b + side_c 
+print("The perimeter of the triangle is :", perimeter)
+#6 : write a script that prompts the user to enter length and width of a rectangle and calculate an area of this rectangle (area = length x width) and perimeter of the rectangle (perimeter = 2 x (length + width))
+length = float(input("Enter length : "))
+width = float(input("Enter width : "))
+area = length * width 
+perimeter = 2 * (length + width)
+print("The area is : " , area)
+print("The perimeter is : " , perimeter)
+#7 : write a script that prompts the user to enter radius of a circle and calculate the area (area = π x r x r) and circumference (c = 2 x π x r) of the circle
 
 
 
